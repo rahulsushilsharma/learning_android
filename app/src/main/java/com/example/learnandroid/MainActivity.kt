65381d65ecf629a6fbf5138f482e844960d1297b
Rahul Sharma <rahu8299@gmail.com>
2023-06-05 19:14:14 +0530

    day one complete

--- app/src/main/java/com/example/learnandroid/MainActivity.kt
+++ app/src/main/java/com/example/learnandroid/MainActivity.kt
@@ -1,7 +1,10 @@
 package com.example.learnandroid
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View.INVISIBLE
+import android.view.View.VISIBLE
 import android.widget.Button
 import android.widget.EditText
 import android.widget.TextView
@@ -14,16 +17,25 @@ class MainActivity : AppCompatActivity() {
         val textVi = findViewById<TextView>(R.id.textView)
         val inp = findViewById<EditText>(R.id.editTextText)
         val butt = findViewById<Button>(R.id.button)
-
+        val butt1 = findViewById<Button>(R.id.button2)
+        var userName = ""
         butt.setOnClickListener{
             if(inp.text.toString() == ""){
                 textVi.text = ""
                 Toast.makeText(this@MainActivity,"Please enter your name",Toast.LENGTH_SHORT).show()
+                butt1.visibility = INVISIBLE
             }else {
-                val msg = "Welcome ${inp.text.toString()}";
+                userName = inp.text.toString()
+                val msg = "Welcome $userName";
                 textVi.text = msg;
                 inp.text.clear();
+                butt1.visibility = VISIBLE
             }
+        }
+        butt1.setOnClickListener{
+            val intent = Intent(this,SecondActivity::class.java)
+            intent.putExtra("USER",userName)
+            startActivity(intent)
         }
 
     }
---
+++ app/src/main/java/com/example/learnandroid/SecondActivity.kt
@@ -0,0 +1,23 @@
+package com.example.learnandroid
+
+import android.os.Bundle
+import android.widget.TextView
+import com.google.android.material.snackbar.Snackbar
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.WindowCompat
+import androidx.navigation.findNavController
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.navigateUp
+import androidx.navigation.ui.setupActionBarWithNavController
+import com.example.learnandroid.databinding.ActivitySecondBinding
+
+class SecondActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_second)
+        val userName = intent.getStringExtra("USER")
+        val text3 = findViewById<TextView>(R.id.textView3)
+        text3.text = userName;
+    }
+}
