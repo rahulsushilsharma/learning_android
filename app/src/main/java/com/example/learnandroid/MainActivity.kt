9ad74eaf72d6ca11d737ec01c6fca22d2367cbdb
Rahul Sharma <rahu8299@gmail.com>
2023-06-07 12:58:35 +0530

    complete day 3

--- app/src/main/java/com/example/learnandroid/MainActivity.kt
+++ app/src/main/java/com/example/learnandroid/MainActivity.kt
@@ -1,30 +1,43 @@
 package com.example.learnandroid
 
 import android.content.Intent
-import androidx.appcompat.app.AppCompatActivity
+import android.content.SharedPreferences
 import android.os.Bundle
+import android.util.Log
 import android.view.View.INVISIBLE
 import android.view.View.VISIBLE
 import android.widget.Button
 import android.widget.EditText
 import android.widget.TextView
 import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var sp: SharedPreferences;
+    private lateinit var editor: SharedPreferences.Editor;
+    private lateinit var textVi: TextView
+    private lateinit var inp: EditText
+    private lateinit var butt: Button
+    private lateinit var butt1: Button
+    private lateinit var userName: String
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        val textVi = findViewById<TextView>(R.id.textView)
-        val inp = findViewById<EditText>(R.id.editTextText)
-        val butt = findViewById<Button>(R.id.button)
-        val butt1 = findViewById<Button>(R.id.button2)
-        var userName = ""
-        butt.setOnClickListener{
-            if(inp.text.toString() == ""){
+        textVi = findViewById<TextView>(R.id.textView)
+        inp = findViewById<EditText>(R.id.editTextText)
+        butt = findViewById<Button>(R.id.button)
+        butt1 = findViewById<Button>(R.id.button2)
+        userName = ""
+        sp = getSharedPreferences("my_sf", MODE_PRIVATE)
+        editor = sp.edit()
+        Log.i("MY MSG", "main activity : onCreate")
+        butt.setOnClickListener {
+            if (inp.text.toString() == "") {
                 textVi.text = ""
-                Toast.makeText(this@MainActivity,"Please enter your name",Toast.LENGTH_SHORT).show()
+                Toast.makeText(this@MainActivity, "Please enter your name", Toast.LENGTH_SHORT)
+                    .show()
                 butt1.visibility = INVISIBLE
-            }else {
+            } else {
                 userName = inp.text.toString()
                 val msg = "Welcome $userName";
                 textVi.text = msg;
@@ -32,11 +45,60 @@ class MainActivity : AppCompatActivity() {
                 butt1.visibility = VISIBLE
             }
         }
-        butt1.setOnClickListener{
-            val intent = Intent(this,SecondActivity::class.java)
-            intent.putExtra("USER",userName)
+        butt1.setOnClickListener {
+            val intent = Intent(this, SecondActivity::class.java)
+            intent.putExtra("USER", userName)
             startActivity(intent)
         }
 
     }
+
+    override fun onStart() {
+        super.onStart()
+        Log.i("MY MSG", "main activity : onStart")
+    }
+
+    override fun onResume() {
+        super.onResume()
+        Log.i("MY MSG", "main activity : onResume")
+        val name = sp.getString("sp_name",null)
+        textVi.text = name;
+        inp.setText(name)
+    }
+
+    override fun onPause() {
+        super.onPause()
+        Log.i("MY MSG", "main activity : onPause")
+        try {
+            val name = userName
+            editor.apply{
+            putString("sp_name", name);
+            commit();
+            }
+        }catch (err:Throwable){
+            Log.i("err",err.toString())
+            inp.setText(err.toString())
+        }
+
+
+    }
+
+    override fun onStop() {
+        super.onStop()
+        Log.i("MY MSG", "main activity : onStop")
+
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        Log.i("MY MSG", "main activity : onDestroy")
+
+    }
+
+    override fun onRestart() {
+        super.onRestart()
+        Log.i("MY MSG", "main activity : onRestart")
+
+    }
+
 }
--- app/src/main/java/com/example/learnandroid/SecondActivity.kt
+++ app/src/main/java/com/example/learnandroid/SecondActivity.kt
@@ -1,23 +1,54 @@
 package com.example.learnandroid
 
 import android.os.Bundle
+import android.util.Log
 import android.widget.TextView
-import com.google.android.material.snackbar.Snackbar
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.WindowCompat
-import androidx.navigation.findNavController
-import androidx.navigation.ui.AppBarConfiguration
-import androidx.navigation.ui.navigateUp
-import androidx.navigation.ui.setupActionBarWithNavController
-import com.example.learnandroid.databinding.ActivitySecondBinding
 
 class SecondActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_second)
+        Log.i("MY MSG" , "sec activity : onCreate")
+
         val userName = intent.getStringExtra("USER")
         val text3 = findViewById<TextView>(R.id.textView3)
         text3.text = userName;
     }
+    override fun onStart() {
+        super.onStart()
+        Log.i("MY MSG" , "sec activity : onStart")
+    }
+
+    override fun onResume() {
+        super.onResume()
+        Log.i("MY MSG" , "sec activity : onResume")
+
+    }
+
+    override fun onPause() {
+        super.onPause()
+        Log.i("MY MSG" , "sec activity : onPause")
+
+    }
+
+    override fun onStop() {
+        super.onStop()
+        Log.i("MY MSG" , "sec activity : onStop")
+
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        Log.i("MY MSG" , "sec activity : onDestroy")
+
+    }
+
+    override fun onRestart() {
+        super.onRestart()
+        Log.i("MY MSG" , "sec activity : onRestart")
+
+    }
+
 }
