be5e9606fe75e0d3866e25c2213ec37248014f11
Rahul Sharma <rahu8299@gmail.com>
2023-06-10 18:47:39 +0530

    .

--- app/src/main/java/com/example/learnandroid/MainActivity2.kt
+++ app/src/main/java/com/example/learnandroid/MainActivity2.kt
@@ -13,28 +13,28 @@ class MainActivity2 : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main2)
-//        var button = findViewById<Button>(R.id.play)
-//        var pause = findViewById<FloatingActionButton>(R.id.iconPause)
-//        var play = findViewById<FloatingActionButton>(R.id.iconPlay)
-//        var stop = findViewById<FloatingActionButton>(R.id.iconStop)
-//        seekBar = findViewById(R.id.seekBar)
-//
-//        play.setOnClickListener {
-//            if(mediaPlayer==null) {
-//                mediaPlayer = MediaPlayer.create(this, R.raw.audio)
-//            }
-//            mediaPlayer?.start()
-//        }
-//        pause.setOnClickListener {
-//            mediaPlayer?.pause()
-//        }
-//        stop.setOnClickListener {
-//            mediaPlayer?.stop()
-//            mediaPlayer?.reset();
-//            mediaPlayer?.release();
-//            mediaPlayer = null
-//
-//        }
+        var button = findViewById<Button>(R.id.play)
+        var pause = findViewById<FloatingActionButton>(R.id.iconPause)
+        var play = findViewById<FloatingActionButton>(R.id.iconPlay)
+        var stop = findViewById<FloatingActionButton>(R.id.iconStop)
+        seekBar = findViewById(R.id.seekBar)
+
+        play.setOnClickListener {
+            if(mediaPlayer==null) {
+                mediaPlayer = MediaPlayer.create(this, R.raw.audio)
+            }
+            mediaPlayer?.start()
+        }
+        pause.setOnClickListener {
+            mediaPlayer?.pause()
+        }
+        stop.setOnClickListener {
+            mediaPlayer?.stop()
+            mediaPlayer?.reset();
+            mediaPlayer?.release();
+            mediaPlayer = null
+
+        }
     }
     private fun initlizeSeeKBar(){
         seekBar.setOnSeekBarChangeListener(object : SeekBar.OnSeekBarChangeListener{
